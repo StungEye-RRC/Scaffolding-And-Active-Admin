0a14c5840ce53ff561ce434e9beaa8c767965617
Kyle Geske <stungeye@gmail.com>
2017-03-13 21:31:57 +0000

    Added the home controller, views, and routes with a product partial too.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  # These two lines are from Active Admin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+
+  # Was generated during scaffolding for our RESTful routes.
   resources :products
-  root 'products#index'
+
+  root 'home#index'
+  get '/show/:id' => 'home#show', id: /\d/
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
